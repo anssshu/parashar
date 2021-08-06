e0d89c619b887298edb2db098ac4660ae60a88d9
anssshu <anssshu@gmail.com>
2021-08-06 21:05:39 +0530

    .chil

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div className="reports">
             <h1>Home</h1>
-        
+            <iframe src="https://itch.io/embed/1058720" width="552" height="167" frameborder="0"><a href="https://anssshu.itch.io/kite">Kite Demo by anshuman mund</a></iframe>
         </div>
         
     )
--- src/pages/chilchila/PageData.js
+++ src/pages/chilchila/PageData.js
@@ -1,6 +1,6 @@
 import React from "react"
-import page1 from "./images/page1.jpg"
-import page2 from "./images/page2.jpg"
+import page1 from "./images/page1.png"
+import page2 from "./images/page2.png"
 
 //village pages
 export const PageData = [
