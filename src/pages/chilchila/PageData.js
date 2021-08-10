5e29906545edae2c9a77ecbe0fbf21db9c0510a0
anssshu <anssshu@gmail.com>
2021-08-10 13:10:57 +0530

    chilchila

--- src/pages/chilchila/PageData.js
+++ src/pages/chilchila/PageData.js
@@ -1,6 +1,22 @@
 import React from "react"
-import page1 from "./images/page1.png"
-import page2 from "./images/page2.png"
+import page1 from "./images/page1.jpg"
+import page2 from "./images/page2.jpg"
+import page3 from "./images/page3.jpg"
+import page4 from "./images/page4.jpg"
+import page5 from "./images/page5.jpg"
+import page6 from "./images/page6.jpg"
+import page7 from "./images/page7.jpg"
+import page8 from "./images/page8.jpg"
+import page9 from "./images/page9.jpg"
+import page10 from "./images/page10.jpg"
+import page12 from "./images/page12.jpg"
+import page13 from "./images/page13.jpg"
+import page11 from "./images/page11.jpg"
+import page15 from "./images/page15.jpg"
+import page20 from "./images/page20.jpg"
+import page21 from "./images/page21.jpg"
+import page22 from "./images/page22.jpg"
+
 
 //village pages
 export const PageData = [
@@ -11,5 +27,66 @@ export const PageData = [
     {
         page:2,
         image:page2
-    }
+    },
+    {
+        page:3,
+        image:page3
+    },
+    {
+        page:4,
+        image:page4
+    },
+    {
+        page:5,
+        image:page5
+    },
+    {
+        page:6,
+        image:page6
+    },
+    {
+        page:7,
+        image:page7
+    },
+    {
+        page:8,
+        image:page8
+    },
+    {
+        page:9,
+        image:page9
+    },
+    {
+        page:10,
+        image:page10
+    },
+    {
+        page:11,
+        image:page11
+    },
+    {
+        page:12,
+        image:page12
+    },
+    {
+        page:13,
+        image:page13
+    },
+    {
+        page:14,
+        image:page15
+    },
+    {
+        page:15,
+        image:page20
+    },
+    {
+        page:16,
+        image:page21
+    },
+    {
+        page:17,
+        image:page22
+    },
+    
 ]
